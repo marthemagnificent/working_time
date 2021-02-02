62f808701169bfb6c4318c9ec092f02d49a6eb1d
marthemagnificent <markaylabrown45@yahoo.com>
2021-02-02 14:39:39 -0500

    added cert of auth

---
+++ working-time.py
@@ -0,0 +1,9 @@
+# Name: Markayla Brown
+# working-time.py
+#
+# Problem: Calculate the working time of employees on a team and outputs them.
+# Certification of Authenticity:
+# <include one of the following>
+# I certify that this assignment is entirely my own work.
+# I certify that this assignment is my own work, but I
+# discussed it with: <Name(s)>
