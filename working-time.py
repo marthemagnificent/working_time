6025e6e32e05ae905db3bf791d106144363b5c3c
marthemagnificent <markaylabrown45@yahoo.com>
2021-02-02 14:58:21 -0500

    added employee and time count

--- working-time.py
+++ working-time.py
@@ -7,3 +7,23 @@
 # I certify that this assignment is entirely my own work.
 # I certify that this assignment is my own work, but I
 # discussed it with: <Name(s)>
+
+
+#variables for time
+hours, minutes = 0, 0
+#ask the user how many employees are on the team
+employees = eval(input("How many employees are on your team? "))
+#loopemp = employees + 1
+#loop to count the employees
+for i in range(employees):
+    #creates a clean number for the sentence asking about each employee
+    empsent = i + 1
+    #sets the times entered to temp values to be added to actual time
+    newHours, newMinutes = eval(input("How much time did employee #" + str(empsent) + " work?"))
+    #adds temp hours to hours accumulator variable
+    hours = hours + newHours
+    #adds temp minutes to the accumulator variable
+    minutes = minutes + newMinutes
+print(hours, minutes)
+#print(loopemp)
+print(empsent)
